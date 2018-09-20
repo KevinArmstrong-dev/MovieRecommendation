e9b0a7255ca184420dcf028fd4b5e3b7a963ca81
Kevin Armstrong Rwigamba <kevin.rwigamba@dawsoncollege.qc.ca>
2018-09-20 09:58:00 -0400

    New Branch

---
+++ Recommendation/Recommendation7/src/movies/PopularRecommender.java
@@ -0,0 +1,120 @@
+/**
+ * 
+ */
+package movies;
+
+/**
+ * @author 1638876
+ *
+ */
+public class PopularRecommender {
+	 private Movie[] movies;
+	 private Rating[] ratings; 
+	 private RecommendAssist [] collection;
+	  
+	 
+	 public PopularRecommender(Rating[] ratings, Movie[] movie) {
+	  this.movies=new Movie[movie.length];
+	  
+	  //to make movies[] immutable and avoid aliasing
+	  for(int i=0;i<movie.length;i++) {
+	   this.movies[i]=movie[i];
+	  }
+	  this.ratings=new Rating[ratings.length];
+	  
+	  //To make ratings[] immutable and avoid aliasing
+	  for(int i=0;i<ratings.length;i++) {
+	   this.ratings[i]=ratings[i];
+	  }
+	  collection=new RecommendAssist [movie.length];
+	  for(int i=0;i<movie.length;i++) {
+	   collection[i]=new RecommendAssist(movie[i],getAverageRatingMovie(movie[i].getId()));
+	  }
+	  
+	 }
+	 
+	 /**This method uses two arrays which are movie and ratings to Calculate
+	  * the average rating of a given movie id
+	  * 
+	  * @param movieId
+	  * @return averageRating
+	  */
+	 public double getAverageRatingMovie(String movieId) {
+	  double averageRating=0;
+	  for(int i=0;i<movies.length;i++) {
+	   for(int x=0;x<ratings.length;x++) {
+	    if(movies[i].getId()==ratings[x].getUserId()) {
+	     averageRating=+ratings[x].getRating();
+	    }
+	   }
+	  }
+	  return averageRating=averageRating/movies.length;
+	  
+	 }
+	 /**Modified Selection Sort to sort the collection array but not tested yet
+	  * or finished
+	  * 
+	  * @param x
+	  */
+	  public static void selectionSort(RecommendAssist[] x) {
+	      for(int start = 0; start < x.length; start++) {
+	        // start is the index of the first element of the 
+	        // unsorted part of the array
+	        
+	        // find the min in the unsorted part
+	        int guess = start; 
+	        for(int i=start+1; i<x.length; i++) {
+	          if(x[guess].getRating() > x[i].getRating()) {
+	            guess =i;
+	          }
+	        }
+	        
+	        // swap the min element with the first element of the
+	        // unsorted part of the array
+	        RecommendAssist temp = x[guess];
+	        x[guess] = x[start];
+	        x[start] = temp;
+	        
+	      }
+	    }
+	/*  public Movie[] recommend(String userid,int n) {
+	   Movie[] temp =new Movie[n];
+	   //This is To count How many movies have not been rated by the given user 
+	   int count=0;
+	   
+	   for(int i=0;i<ratings.length;i++) {
+	    //To find the corresponding user
+		   if(ratings[i].getUserId().equals(userid)) {
+			   for(int x=0;x<movies.length;x++) {
+				   int count1=0;
+				   //this is a comparison to find movies which have not been rated by the user
+				   if(!movies[x].getId().equals(ratings[i].getMovieId())) {
+					   temp[count]=movies[x];
+					   count++;
+					   count1++;
+				   }
+			   }
+		   }
+	   }
+	   //This will make sure the provided number is not more than movies not rated by the user
+	   if(n<count) {
+	  //Since the temporary array will be bigger than the provided number this should work
+	   Movie []unRatedMovies=new Movie[n];
+	    for(int i=0;i<n;i++) {
+	     unRatedMovies[i]=temp[i];
+	    }
+	   }
+	   else {
+	    Movie[] unRatedMovies=new Movie[count];
+	    for(int i=0;i<count;i++) {
+	     unRatedMovies[count]=temp[count];
+	    }
+	   }
+	   return unRatedMovies;
+	  }
+	  */
+
+
+
+
+}
---
+++ Recommendation/Recommendation7/src/movies/RecommendAssist.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package movies;
+
+/**
+ * @author 1638876
+ *
+ */
+public class RecommendAssist {
+	
+	private Movie movie;
+	private double rating;
+	 
+	 /**Helper object to store movies to be recommended
+	  * 
+	  * @param movie
+	  * @param rating
+	  */
+	 public RecommendAssist(Movie movie,double rating) {
+	  
+	  this.movie=movie;
+	  this.rating=rating;
+	 }
+	 
+	 public Movie getMovie() {
+	  return this.movie;
+	 }
+	 public double getRating() {
+	  return rating;
+	 }
+
+}

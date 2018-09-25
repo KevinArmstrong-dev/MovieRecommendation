61e6843b25d8acd1ee43947b5c559b712dc12ee2
Kevin Armstrong Rwigamba <krwigamba@gmail.com>
2018-09-25 00:44:52 -0400

    Added some Testing

--- Recommendation/Recommendation7/test/lib/PopularRecommendTest.java
+++ Recommendation/Recommendation7/test/lib/PopularRecommendTest.java
@@ -17,25 +17,61 @@ public class PopularRecommendTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+		TestPopularRecommender();
+	}
+	
+	/**
+	 * This creates a Testing Rating array
+	 * 
+	 * @return rate
+	 */
+	private static Rating[] createRateArr() {
+		Rating r1 = new Rating("246,810042,2.6,24598");
+		Rating r2 = new Rating("9,1,4,4.5.39");
+		Rating r3 =new Rating("15,1,3.5,997938310");
+		Rating r4 =new Rating("19,11,3,855192773");
+		Rating [] rate= {r1,r2,r3,r4};
+		return rate;
+	}
+	/**
+	 * This creates a Testing Movie array
+	 * 
+	 * @return mvArr
+	 */
+	private static Movie[] createMovieArr() {
+		Movie movieTest= new Movie("11,American President%2C The (1995),Comedy|Drama|Romance|Common");
+		Movie movieTest2=new Movie("1,Toy Story%2C (1995),Adventure|Animation|Children|Comedy|Common");
+		Movie movieTest3=new Movie("2,Jumanji (1995),Adventure|Children|Fantasy|Common");
+		Movie [] mvArr= {movieTest,movieTest2,movieTest3};
+		return mvArr;
+	}
+	/**
+	 * Testing method to evaluate the popular recommend class
+	 * 
+	 */
+
+	private static void TestPopularRecommender(){
 		Rating[] rate=createRateArr();
 		Movie[] mvArr=createMovieArr();
 		PopularRecommender test= new PopularRecommender(rate,mvArr);
 		System.out.println("TEST 1 : Should print every film except: The american president Because it was review by user 19");
-		Movie [] testOne=test.recommendTwo("19",2);
-		 //This should not work and print an error statement.
 		try{
+			Movie [] testOne=test.recommendTwo("19",1);
 		 for(int i=0;i<testOne.length;i++) {
 			 System.out.println(testOne[i].getName());			 }
 		}
 		 catch(NullPointerException e) {
 			 System.out.println("Number of Reviewed movies>Available movies.");
 		 }
-		System.out.println("TEST 2: Should print print a few movies then say that there are no more non-reviewed movies");
-		System.out.println("TEST 3 : Should print the entire movie Array because they all have the genre: Common & it wasn't rated");
+		 catch(ArrayIndexOutOfBoundsException e) {
+			 System.out.println("No match");
+		 }
+		//System.out.println("TEST 2: Should print print a few movies then say that there are no more non-reviewed movies");
+		//System.out.println("TEST 3 : Should print the entire movie Array because they all have the genre: Common & it wasn't rated");
 		 //This should not be able to find a match
 		 try {
-			 Movie[] testThree=test.recommend("200",3, "Common"); 
+			 Movie[] testThree=test.recommendTwo("100",1); 
+			 
 			 for(int i=0;i<testThree.length;i++) {
 				 System.out.println(testThree[i].getName());
 			 }
@@ -46,66 +82,23 @@ public class PopularRecommendTest {
 		 catch(NullPointerException e) {
 			 System.out.println("No match");
 		 }
-	}
-	private static Rating[] createRateArr() {
-		Rating r1 = new Rating("246,810042,2.6,24598");
-		Rating r2 = new Rating("9,1,4,4.5.39");
-		Rating r3 =new Rating("15,1,3.5,997938310");
-		Rating r4 =new Rating("19,11,3,855192773");
-		Rating [] rate= {r1,r2,r3,r4};
-		return rate;
-	}
-	private static Movie[] createMovieArr() {
-		Movie movieTest= new Movie("11,American President%2C The (1995),Comedy|Drama|Romance|Common");
-		Movie movieTest2=new Movie("1,Toy Story%2C (1995),Adventure|Animation|Children|Comedy|Common");
-		Movie movieTest3=new Movie("2,Jumanji (1995),Adventure|Children|Fantasy|Common");
-		Movie [] mvArr= {movieTest,movieTest2,movieTest3};
-		return mvArr;
-	}
-	private static void TestPopularRecommender(){
-		/**
-		// TODO Auto-generated method stub
-		//this removed Line cause is related to some crashes
-		Rating r1 = new Rating("1,31,2.5,1260759144");
-		Rating r0=new Rating("70,88,3,853954814");
-		Rating r2 = new Rating("9,1,4,9.39");
-		Rating r7= new Rating("18,9,3,856007219");
-		Rating r3 =new Rating("15,11,2,997938310");
-		Rating r4 =new Rating("19,11,3,855192773");
-		Rating r5 =new Rating("1,11,2.5,1260759144");
-		Rating r6 =new Rating ("18,11,4,856007279");
-		Rating r8 =new Rating("135,27,3,844996094");
-		Rating [] rate= {r0,r1,r2,r3,r4,r5,r6,r7,r8};
-		String fakeCvs="11,American President%2C The (1995),Comedy|Drama|Romance";
-		Movie movieTest= new Movie(fakeCvs);
-		Movie movieTest0=new Movie("27,Now and Then (1995),Children|Drama");
-		Movie movieTest1= new Movie("9,Sudden Death%2C (1995),Action");
-		Movie movieTest2=new Movie("1,Toy Story%2C (1995),Adventure|Animation|Children|Comedy|Fantasy|Common");
-		Movie movieTest3=new Movie("2,Jumanji (1995),Adventure|Children|Fantasy|Common");
-		Movie movieTest9=new Movie("70,From Dusk Till Dawn (1996),Action|Comedy|Horror|Thriller|Common");
-		Movie movieTest5=new Movie("135,Down Periscope (1996),Comedy|Common");
-		Movie movieTest6=new Movie("27,Now and Then%2C (1995),Children|Drama|Common");
-		Movie movieTest7=new Movie("15,Cutthroat Island (1995),Action|Adventure|Romance|Common");
-		Movie [] mv1= {movieTest5,movieTest2,movieTest,movieTest9,movieTest3,movieTest0,movieTest1,movieTest6,movieTest7,movieTest9};
-	
-		 PopularRecommender test= new PopularRecommender(rate,mv1);
-		 Movie [] xx=test.recommendBeta("9",3);
-		 //Movie [] xx=test.recommendTwo("2",2);
-		 for(int i=0;i<xx.length;i++) {
-			 System.out.println("REcommend KEV: " +xx[i].getName());
-		 } 
-		
-		 System.out.println();
-		Movie[] recommendTest= (test.recommendOne("100","Common"));
-		 for(int i=0;i<recommendTest.length;i++) {
-			 System.out.println("Recommend: " +recommendTest[i].getName());
+		 
+		 try {
+			 Movie[] testFour=test.recommend("19","Revi");
+			 for(int i=0;i<testFour.length;i++) {
+				 if(testFour[i].hasGenre("Commedy")!=true){
+					 System.out.println("Fail");
+				 }
+				 else {
+					 System.out.println("Pass");
+				 }
+		  }
 		 }
-		 //test.recommendOne("1","Fantasy"); 
-		  
-		 
-		 
-		
-		
-		**/
+		 catch(ArrayIndexOutOfBoundsException e) {
+			 System.out.println("No match");
+		 }
+		 catch(NullPointerException e) {
+			 System.out.println("No match");
+		 }
 	}
 }

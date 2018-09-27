26b8caf0c92f587af48fec2a07c1a48b9f5177f0
Franco <franco.g.moro@gmail.com>
2018-09-27 13:00:20 -0400

    created Personalized recommender

---
+++ Recommendation/Recommendation7/src/movies/PersonalizedRecommender.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package movies;
+
+/**
+ * @author Franco G. Moro
+ *
+ */
+public class PersonalizedRecommender {
+	private double[][] usm;
+	private Movie[] movArr;
+	private Rating[] ratArr;
+	
+	public PersonalizedRecommender(Movie[] movArr,Rating[] ratArr) {
+		this.movArr=movArr;
+		this.ratArr=ratArr;
+		int numUsers=findNumUsers(this.ratArr);
+		this.usm=new double[numUsers][numUsers];
+	}
+	private static int findNumUsers(Rating[] ratings) {
+		int count=0;
+		for(int i=0;i<ratings.length;i++) {
+			if(ratings[i].getUserId()>count) {
+				count=ratings[i].getUserId();
+			}
+		}
+		return count;
+	}
+
+}

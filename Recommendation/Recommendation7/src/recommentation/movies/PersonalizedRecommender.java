bf4b5b294d33b9080b33b4d60b727880be385fe7
Franco <franco.g.moro@gmail.com>
2018-10-03 09:35:27 -0400

    Starting the Usm coding

--- Recommendation/Recommendation7/src/recommentation/movies/PersonalizedRecommender.java
+++ Recommendation/Recommendation7/src/recommentation/movies/PersonalizedRecommender.java
@@ -15,73 +15,55 @@ public class PersonalizedRecommender {
 	public PersonalizedRecommender(Movie[] movArr,Rating[] ratArr) {
 		this.movArr=movArr;
 		this.ratArr=ratArr;
-		int numUsers=findNumUsers(this.ratArr);
-		this.usm=new double[numUsers][numUsers];
+		fillUsm();
 		
 	}
-	
-	private static int findNumUsers(Rating[] ratings) {
-		int count=0;
-		for(int i=0;i<ratings.length;i++) {
-			if(ratings[i].getUserId()>count) {
-				count=ratings[i].getUserId();
-			}
-		}
-		return count;
-	}
-	/**
-	 * Alexander Arella & Franco G. Moro
-	 */
 	private void fillUsm() {
-		int[] missingUsers = getMissingUsers();
-		for(int i=0;i<this.usm.length;i++) {
-			if(isMissing(missingUsers,i))continue;
-			int set=i+1;
-			for(int j=0;j<this.usm.length;j++) {
-				if(set>this.usm.length) {
-					set=1;
-				}
-				if(isMissing(missingUsers,i)) {
-					set++;
-					continue;
-				}
-				else {
-					this.usm[i][j]=set;
-					set++;
-				}
+		Rating[][] workTable=getWorkTable(countUsers());
+		for(int i=0;i<workTable.length;i++) {
+			System.out.print("{");
+			for(int j=0;j<workTable[i].length;j++) {
+				System.out.print(workTable[i][j]+ ",");
 			}
+			System.out.print("}");
+			System.out.println();
 		}
+		
+		
 	}
-	
-	private static boolean isMissing(int[] missingUsers,int id) {
-		for(int i=0;i<missingUsers.length;i++) {
-			if(missingUsers[i]==id)return true;
-		}
-		return false;
-	}
-	private int[] getMissingUsers() {
-		int count=0;
-		int position=0;
-		for(int i=0;i<this.ratArr.length;i++) {
-			if(!(isReal(this.ratArr[i].getUserId() ))) {
-				count++;
-			}
-		}
-		int[] output=new int[count];
-		for(int i=0;i<this.ratArr.length;i++) {
-			if(!(isReal(this.ratArr[i].getUserId() ))) {
-				output[position]=this.ratArr[i].getUserId();
-				position++;
-			}
+	private Rating[][] getWorkTable(int nUsers){
+		Rating[][] output= new Rating[nUsers][];
+		for(int i=0;i<=nUsers;i++) {
+			output[i]=getUserRatings(i);
 		}
 		return output;
 		
+		
 	}
-	
-	private boolean isReal(int user) {
-		for(int i=0;i<this.ratArr.length;i++) {
-			if(this.ratArr[i].getRating()==user)return true;
+	private Rating[] getUserRatings(int userId) {
+		int count=0;
+		for(Rating r:this.ratArr) {
+			if(r.getUserId()==userId) {
+				count++;
+			}
 		}
-		return false;
+		Rating[] output=new Rating[count];
+		int pos=0;
+		for(Rating r:this.ratArr) {
+			if(r.getUserId()==userId) {
+				output[pos]=r;
+				pos++;
+			}
+		}
+		return output;
+	}
+	private int countUsers(){
+		int count=0;
+		for(int i=0;i<this.ratArr.length;i++) {
+			if(ratArr[i].getUserId()>count) {
+				count=ratArr[i].getUserId();
+			}
+		}
+		return count;
 	}
 }
---
+++ Recommendation/Recommendation7/test/lib/PersonalizedRecommenderTest.java
@@ -0,0 +1,11 @@
+package lib;
+
+import recommentation.movies.PersonalizedRecommender;
+
+public class PersonalizedRecommenderTest {
+
+	public static void main(String[] args) {
+
+	}
+
+}

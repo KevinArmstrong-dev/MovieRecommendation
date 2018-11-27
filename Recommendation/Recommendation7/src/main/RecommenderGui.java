85fbcc62224962e33487386e079690648cda7cc8
hunter-x-hunter_alpha <hunter-x-hunter_alpha@outlook.com>
2018-11-27 12:10:54 -0500

    Some designed Gui

--- Recommendation/Recommendation7/src/main/RecommenderGui.java
+++ Recommendation/Recommendation7/src/main/RecommenderGui.java
@@ -2,12 +2,24 @@
  * 
  */
 package main;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import javafx.application.*;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 /**
  * @author Kevin Armstrong Rwigamba
@@ -20,16 +32,48 @@ public class RecommenderGui extends Application {
 	public void start(Stage stage) {
 		
 		Group root=new Group();
-		Scene  scene  =  new  Scene(root,  400,  300);        
+		Scene  scene  =  new  Scene(root,  500,  300);        
 		scene.setFill(Color.WHITESMOKE);
 		
 		//Set the title for the application
 		stage.setTitle("Recommender");
-		HBox firstLine=new HBox();
+		VBox container=new VBox();
+		HBox thirdLine=new HBox();
+		HBox fourthLine=new HBox();
+
+		//Radio buttons For Movie or Selection
+		ToggleGroup itemType=new ToggleGroup();
+		RadioButton book=new RadioButton("Book");
+		book.setToggleGroup(itemType);
+		RadioButton movie=new RadioButton("Movie");
+		movie.setToggleGroup(itemType);
 		
+		//file path
+		TextField path =new TextField("/data/..");
+		TextField ratePath=new TextField("/data/..");
+		Label ratings=new Label("Ratings");
+		
+		//Load and Save Button
+		Button load=new Button("Load");
+		Button save=new Button("Save");
+		
+		container.getChildren().addAll(book,movie);
+		//Adding the Third Line to the VBox
 		Label Item=new Label("Item");
-		firstLine.getChildren().add(Item);
-		root.getChildren().add(firstLine);
+		thirdLine.getChildren().addAll(Item,path,ratings,ratePath,load,save);
+		
+		//Adding The fourth Line the Vbox
+		Label userIdLbl=new Label("UserId");
+		TextField userIdTxt=new TextField();
+		//Here we shall add the list view after
+		RadioButton rating0=new RadioButton("0");
+		fourthLine.getChildren().addAll(userIdLbl,userIdTxt,rating0);
+		//Here we shall add the list view after
+		
+		
+		
+		container.getChildren().addAll(thirdLine,fourthLine);
+		root.getChildren().add(container);
 		
 		stage.setScene(scene);       
 		stage.show();

b1f427c14f370276726cdd6bfad687c1b1ffcaf7
Franco G. Moro <franco.g.moro@gmail.com>
2018-11-30 16:42:49 -0500

    Finished the saving methods

--- Recommendation/Recommendation7/src/recommendation/fileio/MovieLensFileReader.java
+++ Recommendation/Recommendation7/src/recommendation/fileio/MovieLensFileReader.java
@@ -42,6 +42,13 @@ public class MovieLensFileReader {
 		}
 		return ratArr;
 	}
+	/**
+	 * Franco G. Moro
+	 * @param objects
+	 * @param filepath
+	 * @param fileHeader
+	 * Saves a copy of an array of Books/Ratings, one could also use the same method from GoodReadsFileReader instead.
+	 */
 	public static <T extends Saveable> void saveToFile(T[] objects,String filepath,String fileHeader) {
 		Path path= Paths.get(filepath);
 		List<String> filecontent=new ArrayList<String>();
--- Recommendation/Recommendation7/test/lib/SaveableTest.java
+++ Recommendation/Recommendation7/test/lib/SaveableTest.java
@@ -1,17 +1,40 @@
 package lib;
 
 import java.io.IOException;
-import java.nio.file.Path;
 
+import recommendation.book.Book;
+import recommendation.fileio.GoodReadsFileReader;
 import recommendation.fileio.MovieLensFileReader;
 import recommendation.movies.Movie;
 import recommendation.movies.Rating;
 
+/**
+ * 
+ * @author Franco G. Moro
+ * Test class that tests the saving for all types.
+ *
+ */
 public class SaveableTest {
+	/**
+	 * 
+	 * @param args
+	 * The main runs a method for all the different tests for each object, the files will be located datafiles/testfiles/test[Object]Copy.csv
+	 */
 	public static void main(String[] args) {
 		MovieTest();
 		RatingTest();
+		BookTest();
 		BookRatingTest();
+	}
+	private static void BookTest() {
+		Book[] bookArr=new Book[0];
+		try {
+			bookArr=GoodReadsFileReader.loadBooks("datafiles/books/books.csv");
+		}
+		catch(IOException e) {
+			System.out.println(e);
+		}
+		MovieLensFileReader.saveToFile(bookArr, "datafiles/testfiles/testBookCopy.csv", "userid,movieid,rating,timestamp");
 	}
 	private static void BookRatingTest() {
 		Rating[] ratArr=new Rating[0];
